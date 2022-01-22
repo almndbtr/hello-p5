735204f19f8002f3d0afc9f780cbc1829c65355f
almndbtr.eth <78528185+almndbtr@users.noreply.github.com>
2022-01-22 21:30:59 +0000

    feat: create animation example

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+// https://p5js.org/examples/hello-p5-animation.html
+const CANVAS_WIDTH = 720
+const CANVAS_HEIGHT = 400
+
+function createCircle () {
+  return {
+    x: 0,
+    y: 0
+  } 
+}
+
+// These are scoped outside so it's accessible to `setup` and `draw`.
+let firstCircle = createCircle()
+let secondCircle = createCircle()
+
 function setup() {
-  createCanvas(400, 400);
+
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+
+  // Set the initial position
+  firstCircle.x = width / 2
+  firstCircle.y = height
 }
 
 function draw() {
-  background(220);
+  background(200)
+
+  // draw a circle
+  stroke(50)
+  fill(100)
+  ellipse(firstCircle.x, firstCircle.y, 24, 24)
+
+  firstCircle.x = firstCircle.x + random(-1, 1)
+  firstCircle.y = firstCircle.y - 1
+
+  if (firstCircle.y < 0) {
+    firstCircle.y = height
+  }
 }
